915d4dffb48610669b82abbff1954b61379ba519
samiullah997 <samiullahk997@gmail.com>
2022-10-06 15:14:21 +0500

    update detail.js file for the the post

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import '../css/details.css';
+import ProptTypes from 'prop-types';
+import HeroContainer from '../components/HeroDetails';
 
-const Details = () => (
-  <h2>Details</h2>
+const Details = ({ slug }) => {
+  const [,, heroSlug] = slug.split('/'); // prop => /heroes/149-captain-america
 
-);
+  const name = heroSlug.slice(heroSlug.indexOf('-') + 1);
 
+  return (
+    <>
+      <div>
+        <HeroContainer hero={name} />
+      </div>
+    </>
+  );
+};
+
+Details.propTypes = {
+  slug: ProptTypes.string.isRequired,
+};
 export default Details;
