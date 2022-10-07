03a91c326e81abe7b7f24d380336f74af16a723b
samiullah997 <samiullahk997@gmail.com>
2022-10-07 20:39:24 +0500

    add Filter functionality

---
+++ src/components/HeroForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { filterHeroes, load20Heroes } from '../redux/heroes/Reducer';
+import '../css/form.css';
+
+const HeroForm = ({ publishers }) => {
+  const [publisher, setPublisher] = useState('');
+  const dispatch = useDispatch();
+  const handleSelect = (filter) => {
+    setPublisher(filter);
+  };
+
+  const handleSubmitPublisher = (e) => {
+    e.preventDefault();
+    dispatch(filterHeroes(publisher));
+    console.log(publisher);
+  };
+
+  const handleDefault20 = () => {
+    setPublisher('');
+    dispatch(load20Heroes([]));
+  };
+  return (
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmitPublisher}>
+        <select
+          className="form-list"
+          name="publisher"
+          label="Filter Heroes"
+          onChange={handleSelect}
+          value={publisher}
+        >
+          {publishers.map((publisher) => (
+            <option value={publisher} key={publisher}>
+              {publisher}
+            </option>
+          ))}
+        </select>
+
+        <div className="form-buttons">
+          <button
+            className="default"
+            type="button"
+            onClick={handleDefault20}
+          >
+            Default 20
+          </button>
+          <button
+            className=""
+            type="submit"
+          >
+            Filter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+HeroForm.propTypes = {
+  publishers: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+export default HeroForm;
